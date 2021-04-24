6a14e9f90a5919448bf7a2068884ae3851934401
Ahmadbinshoaib <ahmadshoaib1044@gmail.com>
2021-04-24 15:13:46 +0500

    Creating files for Tasks

---
+++ task1/src/task1/Task1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Maria
+ */
+public class Task1 {
+
+    /**
+     * @param args the command line arguments
+     */
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+      
+        
+    }
+    
+}
