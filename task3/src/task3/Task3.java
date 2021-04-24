3dc3685e892bd38ee4b82338e1ecdf85fccba4c1
ahtisham774 <ahtishamrajpoot774@gmail.com>
2021-04-24 17:02:44 +0500

    Task3 Impelemeted

--- task3/src/task3/Task3.java
+++ task3/src/task3/Task3.java
@@ -5,18 +5,86 @@
  */
 package task3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Maria
  */
 public class Task3 {
 
+    public static String ridMultipleBlanks(String s) {
+        String newString = "";
+        String[] toks = s.split(" ");
+        for (int i = 0; i < toks.length; i++) {
+            newString = newString + toks[i] + " ";
+
+        }
+
+        return newString;
+    }
+
+    public static String removeInteger(String s) {
+        String newString = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+
+            } else {
+                newString = newString + s.charAt(i);
+            }
+
+        }
+        return newString;
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World");
+       String menu = "Select the Following Option :\n"+"1. For RidMultipleBlanks \n"+
+                     "2. For RemoveTnteger\n"+"3. For stringEncryption\n"+
+                     "0. For Exit\n"+"Enter option ";
+      Scanner input = new Scanner(System.in);
+      String option = "" ;
+      do{
+      System.out.print(menu);
+      option = input.next();
+           switch (option) {
+               case "1":
+                   {
+                       System.out.println("---------------RidMultipleBlanks--------------\n\n");
+                       System.out.println("Input String :");
+                       String line = input.nextLine();
+                       System.out.println(ridMultipleBlanks(line));
+                       break;
+                   }
+               case "2":
+                   {
+                       System.out.println("---------------RemoveInteger--------------\n\n");
+                       System.out.println("Input String :");
+                       String line = input.nextLine();
+                       System.out.println(removeInteger(line));
+                       break;
+                   }
+               case "3":
+                   {
+                       System.out.println("---------------stringEncryption--------------\n\n");
+                       System.out.println("Input String :");
+                       String line = input.nextLine();
+                       System.out.println(ridMultipleBlanks(line));
+                       break;
+                   }
+               case "0":
+                   break;
+               default:
+                   System.out.print("\n Invalid Option \n");
+                   break;
+           }
+      }while(!option.equals("0"));
+      
+       
+       
     }
-    
+
 }
