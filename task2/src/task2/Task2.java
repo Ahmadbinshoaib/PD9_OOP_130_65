c8f9434f78b1d36d3e6ec277f3a559cca7f21955
ahtisham774 <ahtishamrajpoot774@gmail.com>
2021-04-24 16:37:48 +0500

    Task2 displayVertical Funtion added

--- task2/src/task2/Task2.java
+++ task2/src/task2/Task2.java
@@ -1,4 +1,4 @@
-/*
+/**
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -16,5 +16,12 @@ public class Task2 {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    public static String displayVertical(String s) {
+        String vertical_String = "";
+        for (int i = 0; i < s.length(); i++) {
+            vertical_String = vertical_String + s.charAt(i) + "\n";
+        }
+        return vertical_String;
+    }
 }
