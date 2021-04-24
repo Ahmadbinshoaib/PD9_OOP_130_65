bc24973a9e6bdcc638c7f060e3b0de1cecde27d1
ahtisham774 <ahtishamrajpoot774@gmail.com>
2021-04-24 16:47:03 +0500

    Task2 Completed

---
+++ task2/src/Task2.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+/**
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Maria
+ */
+public class Task2 {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static String displayVertical(String s) {
+        String vertical_String = "";
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == ' '){
+            
+            }else{
+            vertical_String = vertical_String + s.charAt(i) + "\n";
+        
+            }
+        }
+        return vertical_String;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a sentence :");
+        String sentence  = input.nextLine();
+        System.out.println("Your sentence printed vertically is : ");
+        System.out.print(displayVertical(sentence));
+    }
+
+   
+}
