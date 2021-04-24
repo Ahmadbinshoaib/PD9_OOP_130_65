2a09b2bb0f06e73e325f5a6df850e4bac83b1e47
Ahmadbinshoaib <ahmadshoaib1044@gmail.com>
2021-04-24 17:16:31 +0500

    SubString

--- task1/src/task1/Task1.java
+++ task1/src/task1/Task1.java
@@ -101,12 +101,33 @@ public class Task1 {
       return TotalPunc;  
     }
     
+      public static boolean findSubString(String s, String sub)
+    { 
+    
+    char[] string= s.toCharArray();
+    char[] Sub=  sub.toCharArray();
+        
+        
+        for (int i = 0; i < string.length - Sub.length+1; i++) {
+        for (int j = 0; j < Sub.length; j++) {
+            if (string[i + j] == Sub[j]) {
+                if (j == Sub.length - 1) {
+                    return true;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+    return false;
+  
+    }
    
     public static void main(String[] args) {
         // TODO code application logic here
         
         String str= JOptionPane.showInputDialog("Please enter the String: ");
-        
+        String sub= JOptionPane.showInputDialog("Please enter the SubString: ");
         
         int totalWords= wordCounter(str);
         int totalVowels= vowelcounter(str);
@@ -115,7 +136,14 @@ public class Task1 {
          System.out.println("Total words in String are: " +totalWords);
          System.out.println("Total vowels in String are: " +totalVowels);
          System.out.println("Total Punctuation Marks in String are: " +TotalPunc);
-        
+         if(findSubString( str,  sub))
+        {
+         System.out.println(sub + " is in the string ");  
+        }
+        else
+        {
+         System.out.println(sub + " is not in the string ");     
+        }
         
       
         
