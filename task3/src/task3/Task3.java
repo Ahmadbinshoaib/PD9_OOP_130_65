90b5fd3d315a08256478e874d0465b74ec739c51
ahtisham774 <ahtishamrajpoot774@gmail.com>
2021-04-25 11:42:29 +0500

    Task3 completed

--- task3/src/task3/Task3.java
+++ task3/src/task3/Task3.java
@@ -5,92 +5,123 @@
  */
 package task3;
 
+import java.util.Random;
 import java.util.Scanner;
 
-/**
- *
- * @author Maria
- */
+/** @author Maria */
 public class Task3 {
 
-    public static String ridMultipleBlanks(String s) {
-        String newString = "";
-        String[] toks = s.split(" ");
-        for (int i = 0; i < toks.length; i++) {
-            if (toks[i].equals("")) {
+  public static String ridMultipleBlanks(String s) {
+    String newString = "";
+    String[] toks = s.split(" ");
+    for (int i = 0; i < toks.length; i++) {
+      if (toks[i].equals("")) {
 
-            } else {
+      } else {
 
-                newString = newString + toks[i] + " ";
-            }
-
-        }
-
-        return newString;
+        newString = newString + toks[i] + " ";
+      }
     }
 
-    public static String removeInteger(String s) {
-        String newString = "";
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+    return newString;
+  }
 
-            } else {
-                newString = newString + s.charAt(i);
-            }
+  public static String removeInteger(String s) {
+    String newString = "";
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
 
-        }
-        return newString;
+      } else {
+        newString = newString + s.charAt(i);
+      }
     }
+    return newString;
+  }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        String menu = "Select the Following Option :\n" + "1. For RidMultipleBlanks \n"
-                + "2. For RemoveTnteger\n" + "3. For stringEncryption\n"
-                + "0. For Exit\n" + "Enter option ";
-        Scanner input = new Scanner(System.in);
-        String option;
-        do {
-            System.out.print(menu);
-            option = input.next();
-            switch (option) {
-                case "1": {
-                    System.out.println("---------------RidMultipleBlanks--------------");
-                    System.out.println("Input String :");
-                    Scanner s = new Scanner(System.in);
-                    String line = s.nextLine();
-                    System.out.println(ridMultipleBlanks(line));
-                    System.out.println();
-                    break;
-                }
-                case "2": {
-                    System.out.println("---------------RemoveInteger--------------");
-                    System.out.println("Input String :");
-                    Scanner s = new Scanner(System.in);
-                    String line = s.nextLine();
-                    System.out.println(removeInteger(line));
-                    System.out.println();
-                    break;
-                }
-                case "3": {
-                    System.out.println("---------------stringEncryption--------------");
-                    /*System.out.println("Input String :");
-                    Scanner s = new Scanner(System.in);
-                    String line = s.nextLine();
-                    System.out.println(ridMultipleBlanks(line));
-                    System.out.println();*/
-                    break;
-                }
-                case "0":
-                    break;
-                default:
-                    System.out.print("\n Invalid Option \n");
-                    break;
-            }
-        } while (!option.equals("0"));
+  public static String stringEncryption(String s) {
+    String upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
+    String numbers = "0123456789";
 
+    // Combine all Strings
+    String alphaNumeric = upperAlphabet + lowerAlphabet + numbers;
+
+    // create random String Builder
+    StringBuilder sb = new StringBuilder();
+
+    // create an object of Random class
+    Random random = new Random();
+
+    // Specify length of random String
+    int length = s.length() / 4;
+
+    for (int i = 0; i < length; i++) {
+
+      // generate random index number
+      int index = random.nextInt(alphaNumeric.length());
+
+      // get character sepecifed by index
+      // from the string
+      char randomChar = alphaNumeric.charAt(index);
+
+      // append the character to string builder
+      sb.append(randomChar);
     }
-
+    String randomString = sb.toString();
+    return randomString;
+  }
+  /** @param args the command line arguments */
+  public static void main(String[] args) {
+    // TODO code application logic here
+    String menu =
+        "Select the Following Option :\n"
+            + "1. For RidMultipleBlanks \n"
+            + "2. For RemoveTnteger\n"
+            + "3. For stringEncryption\n"
+            + "0. For Exit\n"
+            + "Enter option ";
+    Scanner input = new Scanner(System.in);
+    String option;
+    do {
+      System.out.print(menu);
+      option = input.next();
+      switch (option) {
+        case "1":
+          {
+            System.out.println("---------------RidMultipleBlanks--------------");
+            System.out.println("Input String :");
+            Scanner s = new Scanner(System.in);
+            String line = s.nextLine();
+            System.out.println(ridMultipleBlanks(line));
+            System.out.println();
+            break;
+          }
+        case "2":
+          {
+            System.out.println("---------------RemoveInteger--------------");
+            System.out.println("Input String :");
+            Scanner s = new Scanner(System.in);
+            String line = s.nextLine();
+            System.out.println(removeInteger(line));
+            System.out.println();
+            break;
+          }
+        case "3":
+          {
+            System.out.println("---------------stringEncryption--------------");
+            System.out.println("Input String :");
+            Scanner s = new Scanner(System.in);
+            String line = s.nextLine();
+            System.out.println(stringEncryption(line));
+            System.out.println();
+            break;
+          }
+        case "0":
+          break;
+        default:
+          System.out.print("\n Invalid Option \n");
+          break;
+      }
+    } while (!option.equals("0"));
+  }
 }
